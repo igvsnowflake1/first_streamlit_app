fc4a48e6d219586be33fedddee72db44b8e68c8a
igvsnowflake1 <150789641+igvsnowflake1@users.noreply.github.com>
2023-11-15 12:12:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,5 +28,5 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response)
 streamlit.dataframe(fruityvice_normalized)
