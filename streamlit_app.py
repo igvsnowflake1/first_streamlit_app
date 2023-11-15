205ce3e3883ac548698862309cafb43bea1fb3f2
igvsnowflake1 <150789641+igvsnowflake1@users.noreply.github.com>
2023-11-15 12:04:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,5 +29,4 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response.json())
 
 
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+
