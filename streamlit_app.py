9a7ce4705dc01f1a85f5a709b0174a5e02845dd4
igvsnowflake1 <150789641+igvsnowflake1@users.noreply.github.com>
2023-11-15 16:04:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+st.header("Add a New Fruit")
+
+new_fruit = st.text_input('Enter a new fruit:')
+if new_fruit:
+    st.write('You entered:', new_fruit)
+    # Perform any desired operations with the new fruit, such as adding it to the database.
+
+
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
@@ -29,7 +37,6 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json()) 
 streamlit.dataframe(fruityvice_normalized)
