aff4cf8c7840f43b5b32917194d97bec7b1ec49a
igvsnowflake1 <150789641+igvsnowflake1@users.noreply.github.com>
2023-11-15 16:39:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,9 +35,10 @@ streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
 
-
-
-
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * from fruit_load_list")
+my_data_row = my_cur.fetchone()
 
 streamlit.header("Añadir una Nueva Fruta")
 
@@ -51,9 +52,5 @@ if new_fruit:
     # Convert the list back to a tuple
     my_data_row = tuple(my_data_row_list)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchone()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row_list)
